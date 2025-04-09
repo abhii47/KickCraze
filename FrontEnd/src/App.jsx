f547259e8a245f04c1d18659086168e5c42b8355
abhii47 <150163008+abhii47@users.noreply.github.com>
2025-04-10 02:26:26 +0530

    adding routes concept inside the app.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,28 +1,44 @@
-import React from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import Product from './components/Product'
-import Chatbot from './components/Chatbot'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Product from './components/Product';
+import Chatbot from './components/Chatbot';
+import ProductDetail from './components/Product_Details';
 
 function App() {
   return (
-    <div className=' container bg-zinc-100' >
-      <div className='w-[100%] fixed top-0'>
-        <Navbar />
+    <Router>
+      <div className="container bg-zinc-100">
+        <div className="w-[100%] fixed top-0 z-10">
+          <Navbar />
+        </div>
+
+        <div className="pt-40">
+          <Routes>
+            {/* Homepage with all products */}
+            <Route
+              path="/"
+              element={
+                <div className="main-product-container flex flex-wrap gap-4 justify-center pt-10">
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Chatbot />
+                </div>
+              }
+            />
+
+            {/* Product Details Page */}
+            <Route path="/product_Details" element={<ProductDetail />} />
+          </Routes>
+        </div>
       </div>
-      <div className='main-product-container flex flex-wrap pt-40'>
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Chatbot />
-      </div>
-    </div>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
