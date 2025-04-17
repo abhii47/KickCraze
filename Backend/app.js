6312fe720c0409d55f8961f4647a374dd11ca20b
abhii47 <150163008+abhii47@users.noreply.github.com>
2025-04-18 00:30:12 +0530

    backend express setup created

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) =>{
-   console.log("request occur successsfully"); 
+    if(req === '/')
+    {
+        res.setHeader("content-type","text/html");
+        fs.readFile('./Frontend/src/App.jsx',(err, data)=>{
+            if(err){
+                res.write(`<p>error</p>`);
+                res.end();
+            }else{
+                res.write(data);
+                res.end();
+            }
+        });
+    } 
 });
 
-const port = 5173;
+const port = 3000;
 server.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
